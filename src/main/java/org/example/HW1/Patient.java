c07333c836dc28db02d7c60d1076c9e245e7f85e
Senina Olga <senina@reshenie-soft.ru>
2025-04-07 13:09:15 +0300

    Fixed printPatient method

--- src/main/java/org/example/HW1/Patient.java
+++ src/main/java/org/example/HW1/Patient.java
@@ -2,7 +2,7 @@ package org.example.HW1;
 
 public class Patient implements Action {
 
-    public static int id = 0;
+    private static int id = 0;
     public int age;
     public String lastname;
     public String name;
@@ -10,5 +10,20 @@ public class Patient implements Action {
 
     public int getId() {
         return id;
+    }
+
+    public Patient(String lastname, String name, String patronym, int age) {
+        id++;
+        this.lastname = lastname;
+        this.name = name;
+        this.patronym = patronym;
+        this.age = age;
+        this.id = getId();
+    }
+
+    public void printPatient() {
+        System.out.println("\n" + "Patient: " + lastname + " " + name + " " + patronym +"\n" +
+                "Age: " + age + " years" + "\n" +
+                "Patient id: " + getId() + "\n");
     }
 }
